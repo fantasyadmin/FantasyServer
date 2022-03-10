ccfc2ff98dbb9170e08f06285df906c614235cd7
Gal <Gal@DESKTOP-0UTOF6I>
2022-03-10 23:45:48 +0200

    2

---
+++ WebAPI/Controllers/LogInController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary2;
+
+namespace WebAPI.Controllers
+{
+    public class LogInController : ApiController
+    {
+        bgroup89_test2Entities db = new bgroup89_test2Entities();
+
+        // GET: api/LogIn
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/LogIn/5
+        public bool Get(string inputEmail, string inputPassword)
+        {
+            bool isRegisterd = false;
+            User userEmail = db.User.Where(e => e.email.Equals(inputEmail)).FirstOrDefault();
+            User userPassword = db.User.Where(p => p.password.Equals(inputPassword)).FirstOrDefault();
+            if (userEmail != null && userPassword != null)
+            {
+                isRegisterd = true;
+            }
+            return isRegisterd;
+        }
+
+        // POST: api/LogIn
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT: api/LogIn/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/LogIn/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPI/Controllers/RegisterController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary2;
+
+namespace WebAPI.Controllers
+{
+    public class RegisterController : ApiController
+    {
+        bgroup89_test2Entities db = new bgroup89_test2Entities();
+        // GET: api/Register
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Register/email,pass
+        public string Get(int id)
+        {
+            return "Banana";
+        }
+
+        // POST: api/Register
+        public string Post([FromBody] User user)
+        {
+            try
+            {
+                User u = new User() { email = user.email, password = user.password };
+                db.User.Add(u);
+                db.SaveChanges();
+                return "ok";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        // PUT: api/Register/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Register/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
