818699824860d056b1d8bdd3d9ce7a4fa3c2e77c
Gal <Gal@DESKTOP-0UTOF6I>
2022-05-15 12:04:14 +0300

    15/5 - edit match + delete match

--- WebAPI/Controllers/MatchController.cs
+++ WebAPI/Controllers/MatchController.cs
@@ -40,64 +40,64 @@ namespace WebAPI.Controllers
             try
             {
                 Match match = JsonConvert.DeserializeObject<Match>(matchData.ToString());
-            League league = JsonConvert.DeserializeObject<League>(matchData.ToString());
+                League league = JsonConvert.DeserializeObject<League>(matchData.ToString());
 
-            League l1 = db.League.Where(l => l.league_id == league.league_id).FirstOrDefault();
-            if (l1 == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound, $"League {league.league_id} not found");
-            }
+                League l1 = db.League.Where(l => l.league_id == league.league_id).FirstOrDefault();
+                if (l1 == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, $"League {league.league_id} not found");
+                }
 
-            string strhDate = matchData["match_date"].ToString(); //Convert.ToDateTime(match.match_date);
-            string strTime = matchData["match_time"].ToString();
-            //var strLocation = string.Format("POINT({0})", matchData["location"].ToString());
-            //int intLocation = Convert.ToInt32(matchData["location"]);
-            string strLocation = matchData["location"].ToString();
+                string strhDate = matchData["match_date"].ToString(); //Convert.ToDateTime(match.match_date);
+                string strTime = matchData["match_time"].ToString();
+                //var strLocation = string.Format("POINT({0})", matchData["location"].ToString());
+                //int intLocation = Convert.ToInt32(matchData["location"]);
+                string strLocation = matchData["location"].ToString();
 
-            DateTime matchDate = DateTime.Parse(strhDate);
-            TimeSpan matchTime = TimeSpan.Parse(strTime);
-            System.Data.Entity.Spatial.DbGeography matchLocation = System.Data.Entity.Spatial.DbGeography.FromText("POINT(-122.336106 47.605049)");
+                DateTime matchDate = DateTime.Parse(strhDate);
+                TimeSpan matchTime = TimeSpan.Parse(strTime);
+                System.Data.Entity.Spatial.DbGeography matchLocation = System.Data.Entity.Spatial.DbGeography.FromText("POINT(-122.336106 47.605049)");
 
-            Match m1 = new Match()
-            {
-                match_date = matchDate,
-                match_time = matchTime,
-                location = matchLocation,
-                team_color1 = match.team_color1,
-                team_color2 = match.team_color2,
-                league_id = l1.league_id
-            };
+                Match m1 = new Match()
+                {
+                    match_date = matchDate,
+                    match_time = matchTime,
+                    location = matchLocation,
+                    team_color1 = match.team_color1,
+                    team_color2 = match.team_color2,
+                    league_id = l1.league_id
+                };
 
-            //TimeSpan time = TimeSpan.Parse("07:35");
+                //TimeSpan time = TimeSpan.Parse("07:35");
 
-            db.Match.Add(m1);
-            db.SaveChanges();
+                db.Match.Add(m1);
+                db.SaveChanges();
 
-            //after the game
-            //Active_in ac1 = new Active_in()
-            //{
-            //    user_id = u1.user_id,
-            //    attending = 1,
-            //    pen_missed = active_In.pen_missed,
-            //    goals_scored = active_In.goals_scored,
-            //    goals_recieved = active_In.goals_recieved,
-            //    apporval_status = active_In.apporval_status,
-            //    match_color = active_In.match_color,
-            //    assists = active_In.assists,
+                //after the game
+                //Active_in ac1 = new Active_in()
+                //{
+                //    user_id = u1.user_id,
+                //    attending = 1,
+                //    pen_missed = active_In.pen_missed,
+                //    goals_scored = active_In.goals_scored,
+                //    goals_recieved = active_In.goals_recieved,
+                //    apporval_status = active_In.apporval_status,
+                //    match_color = active_In.match_color,
+                //    assists = active_In.assists,
 
-            //};
+                //};
 
 
-            return Request.CreateResponse(HttpStatusCode.OK, new
-            {
-                m1.match_id,
-                m1.match_date,
-                m1.match_time,
-                m1.location,
-                m1.team_color1,
-                m1.team_color2,
-                m1.league_id
-            }, JsonMediaTypeFormatter.DefaultMediaType);
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    m1.match_id,
+                    m1.match_date,
+                    m1.match_time,
+                    m1.location,
+                    m1.team_color1,
+                    m1.team_color2,
+                    m1.league_id
+                }, JsonMediaTypeFormatter.DefaultMediaType);
             }
             catch (Exception e)
             {
@@ -109,43 +109,43 @@ namespace WebAPI.Controllers
         }
 
         // PUT: api/Match/5
-        //public HttpResponseMessage Put(JObject matchData)
-        //{
-        //    try
-        //    {
-        //        Match match = JsonConvert.DeserializeObject<Match>(matchData.ToString());
+        public HttpResponseMessage Put(JObject matchData)
+        {
+            try
+            {
+                Match match = JsonConvert.DeserializeObject<Match>(matchData.ToString());
 
-        //        Match m1 = db.Match.Where(m => m.match_id == match.match_id).FirstOrDefault();
+                Match m1 = db.Match.Where(m => m.match_id == match.match_id).FirstOrDefault();
 
-        //        if (m1 == null)
-        //        {
-        //            return Request.CreateResponse(HttpStatusCode.NotFound, $"Match {match.match_id} was not found");
-        //        }
+                if (m1 == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, $"Match {match.match_id} was not found");
+                }
 
-        //        m1.match_date = match.match_date;
-        //        m1.match_time = match.match_time;
-        //        m1.location = match.location;
-        //        m1.team_color1 = match.team_color1;
-        //        m1.team_color2 = match.team_color2;
-        //        db.SaveChanges();
+                m1.match_date = match.match_date;
+                m1.match_time = match.match_time;
+                //m1.location = match.location;
+                m1.team_color1 = match.team_color1;
+                m1.team_color2 = match.team_color2;
+                db.SaveChanges();
 
-        //        return Request.CreateResponse(HttpStatusCode.OK, new
-        //        {
-        //            m1.match_id,
-        //            m1.match_date,
-        //            m1.match_time,
-        //            m1.location,
-        //            m1.team_color1,
-        //            m1.team_color2,
-        //            m1.league_id
-        //        }, JsonMediaTypeFormatter.DefaultMediaType);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        logger.Error("Bad Request" + e);
-        //        return Request.CreateResponse(HttpStatusCode.BadRequest, e.InnerException);
-        //    }
-
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    m1.match_id,
+                    m1.match_date,
+                    m1.match_time,
+                    //m1.location,
+                    m1.team_color1,
+                    m1.team_color2,
+                    m1.league_id
+                }, JsonMediaTypeFormatter.DefaultMediaType);
+            }
+            catch (Exception e)
+            {
+                logger.Error("Bad Request" + e);
+                return Request.CreateResponse(HttpStatusCode.BadRequest, e.InnerException);
+            }
+        }
 
 
         //}
@@ -153,37 +153,40 @@ namespace WebAPI.Controllers
         // DELETE: api/Match/5
         //Delete Match
         //recive match_id. return ActiveIn
-        //public HttpResponseMessage Delete(JObject matchData)
-        //{
-        //    try
-        //    {
-        //        Match match = JsonConvert.DeserializeObject<Match>(matchData.ToString());
+        public HttpResponseMessage Delete(JObject matchData)
+        {
+            try
+            {
+                Match match = JsonConvert.DeserializeObject<Match>(matchData.ToString());
 
-        //        Match m1 = db.Match.Where(m => m.match_id == match.match_id).FirstOrDefault();
+                Match m1 = db.Match.Where(m => m.match_id == match.match_id).FirstOrDefault();
 
-        //        if (m1 == null)
-        //        {
-        //            return Request.CreateResponse(HttpStatusCode.NotFound, $"Match {match.match_id} was not found");
-        //        }
+                if (m1 == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, $"Match {match.match_id} was not found");
+                }
 
-        //        db.Match.Remove(m1);
-        //        db.SaveChanges();
+                db.Match.Remove(m1);
+                db.SaveChanges();
 
-        //        return Request.CreateResponse(HttpStatusCode.OK, new
-        //        {
-        //            m1.match_id,
-        //            m1.match_date,
-        //            m1.match_time,
-        //            m1.location,
-        //            m1.league_id
-        //        }, JsonMediaTypeFormatter.DefaultMediaType);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        logger.Error("Bad Request" + e);
-        //        return Request.CreateResponse(HttpStatusCode.BadRequest, e.InnerException);
-        //    }
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
 
-        //}
+                    m1.match_id,
+                    m1.match_date,
+                    m1.match_time,
+                    m1.location,
+                    m1.league_id
+
+                }, JsonMediaTypeFormatter.DefaultMediaType);
+            }
+            catch (Exception e)
+            {
+                logger.Error("Bad Request" + e);
+                return Request.CreateResponse(HttpStatusCode.BadRequest, e);
+            }
+
+        }
+
     }
 }
